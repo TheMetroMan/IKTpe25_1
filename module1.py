620ff27235997b6c47aba5fb3d6b89afb9fbaa75
opilane <opilane@HP-CZC2349HV0>
2025-09-25 11:48:30 +0300

    fuc

---
+++ module1.py
@@ -0,0 +1,9 @@
+print("saame tutavaks!")
+nimi = input("Mis su nimi on? ").capitalize() # sõna
+print(f"Tere, {nimi}! Tore sind kohata.")
+vanus = int(input("Kui vana sa oled? ")) # täisarv
+print(f"{vanus} aastat on väga hea vanus!")
+print(f"järgmine aasta oled sa juba {vanus + 1} aastat vana.")
+pikkus = float(input("Kui pikk sa oled (meeterdes)? ")) # ujukomaarv
+print(f"{pikkus} meetrit on päris pikk!") 
+
